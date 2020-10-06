c65efd3e5bb8261bcebd1fa78883d46141a5f74c
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-10-05 23:27:27 -0700

    take out the slow method

--- p1/Project1.java
+++ p1/Project1.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.annotation.Target;
 import java.util.*;
 
 public class Project1 {
@@ -27,18 +26,7 @@ public class Project1 {
             int num = sc.nextInt();
             targets.add(num);
         }
-        for (Integer target : targets) {
-            System.out.print(target + " ");
-            if (checkBad(values, target)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
 
-            }
-
-        }
-        System.out.println();
-        System.out.println();
         for (Integer target : targets) {
             System.out.print(target + " ");
             if (check(map, values, target)) {
@@ -47,23 +35,6 @@ public class Project1 {
                 System.out.println("NO");
             }
         }
-    }
-
-    private static boolean checkBad(ArrayList<Integer> values, int target) {
-        for (int i = 0; i < values.size(); i++) {
-            for (int j = 0; j < values.size(); j++) {
-                if (i == j)
-                    continue;
-                for (int k = 0; k < values.size(); k++) {
-                    if (i == k || k == j)
-                        continue;
-                    if (values.get(i) + values.get(j) + values.get(k) == target) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
     }
 
     private static boolean check(HashMap<Integer, Integer> map, ArrayList<Integer> values, int sum) {
