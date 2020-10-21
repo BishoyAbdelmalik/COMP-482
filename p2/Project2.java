36e94fd385ca0f7cfcc8d68fdcb424847706d8ad
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-10-20 22:28:23 -0700

    started p2

---
+++ p2/Project2.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+public class Project2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input1.txt"));
+
+    }
+    
+}
