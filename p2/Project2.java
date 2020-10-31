fe5bda808fc99d3380be370f8f63b6bee96b45c9
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-10-30 18:58:18 -0700

    fixed error

--- p2/Project2.java
+++ p2/Project2.java
@@ -1,8 +1,8 @@
 /**
 * Project 2
-* reads a set of data (lattice points - ordered pairs with both coordinates integer) from a file, 
-* stores that data into a data structure, and finds a lattice point which is “most central” to 
-* the data set and the total distance to a lattice point which is “most central” to the data 
+* reads a set of data (lattice points - ordered pairs with both coordinates integer) from a file,
+* stores that data into a data structure, and finds a lattice point which is most central to
+* the data set and the total distance to a lattice point which is most central to the data 
 * using both the L1(ie Manhattan) and L2 (ie normal distance) metrics.
 *
 * @author  Bishoy Abdelmalik
