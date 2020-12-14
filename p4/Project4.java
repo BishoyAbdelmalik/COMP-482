f26bc52e5727040184569a6dd79f23d637f2f9b7
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-12-14 11:54:28 -0800

    change indentation type

--- p4/Project4.java
+++ p4/Project4.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Project4 {
-    public static void main(String[] args) throws FileNotFoundException  {
-        Scanner sc = new Scanner(new File("input4.txt"));
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-        int[][] data=new int[r][c];
-        int[][] path=new int[r][c];
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                data[i][j]=sc.nextInt();
-                path[i][j]=-1;
-            }
-        }
-        
-    }
+	public static void main(String[] args) throws FileNotFoundException  {
+		Scanner sc = new Scanner(new File("input4.txt"));
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		int[][] data=new int[r][c];
+		int[][] path=new int[r][c];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[0].length; j++) {
+				data[i][j]=sc.nextInt();
+				path[i][j]=-1;
+			}
+		}
+
+	}
 }
